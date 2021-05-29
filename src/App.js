b7fd41c9cf8cf4124e5da516eadbf87394d09803
poornesh-pH <poornesh9626@gmail.com>
2021-05-29 12:04:29 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
-import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css';
 import Dashboard from './Components/Dashboard';
+import AppRouter from './Router/AppRouter';
 export default function App() {
   return (
     <div className="container">
       <h5>Chotu Kart </h5>
       <title>Chotu Kart </title>
+      <AppRouter />
       <Dashboard />
     </div>
   );
--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Admin from '././Components/Admin';
-import Cart from '././Components/Cart';
-import Customer from '././Components/Customer';
+import Admin from './../Components/Admin';
+import Cart from './../Components/Cart';
+import Customer from './../Components/Customer';
 
-const AppRouter = () => {
+export default function AppRouter() {
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Customer} />
