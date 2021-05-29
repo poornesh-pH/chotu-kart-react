58c6f8c3bf6955b730832f6744fac6facc78f2ed
poornesh-pH <poornesh9626@gmail.com>
2021-05-29 17:05:48 +0530

    3 files modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ShoppingCartTwoToneIcon from '@material-ui/icons/ShoppingCartTwoTone';
+import BuildIcon from '@material-ui/icons/Build';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+
 export default function Header() {
   return (
     <div className="header">
       <title>Chotu Kart </title>
-      <h5 className="logo">Chotu Kart </h5>
+      <h5 className="logo">Chotu Kart</h5>
+
       <div className="navbar">
         <NavLink exact activeClassName="is-active" to="/">
-          Dashboard
+          Dashboard <DashboardIcon color="primary" fontSize="small" />
         </NavLink>
         <NavLink activeClassName="is-active" to="/admin">
-          Admin
+          Admin <BuildIcon color="primary" fontSize="small" />
         </NavLink>
         <NavLink activeClassName="is-active" to="/cart">
-          Cart
+          Cart <ShoppingCartTwoToneIcon color="primary" fontSize="small" />
         </NavLink>
       </div>
     </div>
