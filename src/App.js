fa3862484d8a3120e96f3907b86d0d43c5284c85
poornesh-pH <poornesh9626@gmail.com>
2021-05-29 12:07:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './style.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import Dashboard from './Components/Dashboard';
-import AppRouter from './Router/AppRouter';
 export default function App() {
   return (
     <div className="container">
       <h5>Chotu Kart </h5>
       <title>Chotu Kart </title>
-      <AppRouter />
       <Dashboard />
     </div>
   );
