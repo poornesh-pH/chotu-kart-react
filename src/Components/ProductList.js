8dc2e4d97056ee40ad73e6ec00ba695a71278bee
poornesh-pH <poornesh9626@gmail.com>
2021-05-30 20:55:33 +0530

    2 files modified

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -42,22 +42,24 @@ export default function ProductList(props) {
       {props.filteredProducts.map(product => {
         return (
           <div key={product.id}>
-            <div className="row">
-              <div className="col s12 m6">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={product.img} />
-                    <a className="btn-floating halfway-fab waves-effect waves-light red">
-                      <i className="material-icons">add</i>
-                    </a>
-                  </div>
-                  <div className="card-content">
-                    <span className="card-title">{product.name}</span>
-                    <p>
-                      I am a very simple card. I am good at containing small
-                      bits of information. I am convenient because I require
-                      little markup to use effectively.
-                    </p>
+            <div className="col s12 m6">
+              <div className="row">
+                <div className="col s12 m12">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={product.img} />
+                      <a className="btn-floating halfway-fab waves-effect waves-light red">
+                        <i className="material-icons">add</i>
+                      </a>
+                    </div>
+                    <div className="card-content">
+                      <span className="card-title">{product.name}</span>
+                      <p>
+                        I am a very simple card. I am good at containing small
+                        bits of information. I am convenient because I require
+                        little markup to use effectively.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
