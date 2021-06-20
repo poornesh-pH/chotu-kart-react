aa880999e20493b4184d80b45f9b6361f8ff898c
poornesh-pH <poornesh9626@gmail.com>
2021-06-20 20:43:05 +0530

    1 file modified

--- src/Components/BooksForm.js
+++ src/Components/BooksForm.js
@@ -11,50 +11,60 @@ class BooksForm extends Component {
               <label for="name">Name</label>
             </div>
             <div class="input-field col s6">
+              <textarea
+                id="description"
+                name="description"
+                class="materialize-textarea"
+              />
+              <label for="description">Description</label>
+            </div>
+            <div class="input-field col s6">
               <input id="name" type="number" name="price" class="validate" />
               <label for="name">Price</label>
             </div>
-          </div>
-          <div className="row">
-          <div class="input-field col s6">
-            <textarea
-              id="description"
-              name="description"
-              class="materialize-textarea"
-            />
-            <label for="description">Description</label>
-          </div>
-          </div>
-          <div className="row">
-            <div class="input-field col s6">
-              <input id="author" name="author" type="text" class="validate" />
-              <label for="author">Author</label>
-            </div>
-            <div class="input-field col s6">
-              <input id="type" name="type" type="text" class="validate" />
-              <label for="type">Type</label>
-            </div>
-          </div>
-
-          <div className="row">
-            <div class="input-field col s6">
-              <input id="img" name="img" type="text" class="validate" />
-              <label for="img">Image URL</label>
-            </div>
             <div class="input-field col s6">
               <input
-                id="category"
-                name="category"
-                type="text"
+                id="stocked"
+                type="number"
+                name="stocked"
                 class="validate"
               />
-              <label for="category">Category</label>
+              <label for="stocked">Stocked</label>
             </div>
-          </div>
 
-          <div class="input-field col s6">
-            <input id="stocked" type="number" name="stocked" class="validate" />
-            <label for="stocked">Stocked</label>
+            <div className="row">
+              <div class="input-field col s6">
+                <input id="author" name="author" type="text" class="validate" />
+                <label for="author">Author</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="type" name="type" type="text" class="validate" />
+                <label for="type">Type</label>
+              </div>
+            </div>
+
+            <div className="row">
+              <div class="input-field col s6">
+                <input id="img" name="img" type="text" class="validate" />
+                <label for="img">Image URL</label>
+              </div>
+              <div class="input-field col s6">
+                <input
+                  id="category"
+                  name="category"
+                  type="text"
+                  class="validate"
+                />
+                <label for="category">Category</label>
+              </div>
+            </div>
+            <button
+              class="btn waves-effect waves-light col s12"
+              type="submit"
+              name="action"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
