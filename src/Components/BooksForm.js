847482a22f52e34e8297ccd81ea252f5c6d23cb4
poornesh-pH <poornesh9626@gmail.com>
2021-06-20 20:30:16 +0530

    1 file modified

--- src/Components/BooksForm.js
+++ src/Components/BooksForm.js
@@ -5,11 +5,18 @@ class BooksForm extends Component {
     return (
       <div>
         <form>
-          <div class="input-field col s6">
-            <input id="name" type="text" class="validate" />
-            <label for="name">Name</label>
+          <div className="row">
+            <div class="input-field col s6">
+              <input id="name" type="text" name="name" class="validate" />
+              <label for="name">Name</label>
+            </div>
+            <div class="input-field col s6">
+              <input id="name" type="number" name="price" class="validate" />
+              <label for="name">Price</label>
+            </div>
           </div>
-          <div class="input-field">
+          <div className="row">
+          <div class="input-field col s6">
             <textarea
               id="description"
               name="description"
@@ -17,9 +24,37 @@ class BooksForm extends Component {
             />
             <label for="description">Description</label>
           </div>
-          <div class="input-field">
-            <input id="name" type="text" class="validate" />
-            <label for="name">Name</label>
+          </div>
+          <div className="row">
+            <div class="input-field col s6">
+              <input id="author" name="author" type="text" class="validate" />
+              <label for="author">Author</label>
+            </div>
+            <div class="input-field col s6">
+              <input id="type" name="type" type="text" class="validate" />
+              <label for="type">Type</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div class="input-field col s6">
+              <input id="img" name="img" type="text" class="validate" />
+              <label for="img">Image URL</label>
+            </div>
+            <div class="input-field col s6">
+              <input
+                id="category"
+                name="category"
+                type="text"
+                class="validate"
+              />
+              <label for="category">Category</label>
+            </div>
+          </div>
+
+          <div class="input-field col s6">
+            <input id="stocked" type="number" name="stocked" class="validate" />
+            <label for="stocked">Stocked</label>
           </div>
         </form>
       </div>
