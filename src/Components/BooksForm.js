800c3832f3290df821d2fef2d6b76da7c9c87a66
poornesh-pH <poornesh9626@gmail.com>
2021-06-23 13:42:29 +0530

    1 file modified

--- src/Components/BooksForm.js
+++ src/Components/BooksForm.js
@@ -1,84 +1,102 @@
 import React, { Component } from 'react';
 
 class BooksForm extends Component {
-  state = {
-    description: '',
-    name: '',
-    price: '',
-    author: '',
-    type: '',
-    img: '',
-    category: '',
-    stocked: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: '',
+      name: '',
+      price: '',
+      author: '',
+      type: '',
+      img: '',
+      category: '',
+      stocked: ''
+    };
+  }
+  onFormSubmit = e => {};
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
-    const onFormSubmit = e => {
-      e.preventDefault();
-      console.log(e.target.value)
-      this.setState({
-        // [e.target.name]: e.target.value
-      });
-    };
-    console.log(this.state)
     return (
       <div>
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={this.onFormSubmit}>
           <div className="row">
-            <div class="input-field col s6">
-              <input id="name" type="text" name="name" class="validate" />
-              <label for="name">Name</label>
+            <div className="input-field col s6">
+              <input
+                id="name"
+                type="text"
+                name="name"
+                onChange={this.handleChange}
+                className="validate"
+              />
+              <label htmlFor="name">Name</label>
             </div>
-            <div class="input-field col s6">
+            <div className="input-field col s6">
               <textarea
                 id="description"
                 name="description"
-                class="materialize-textarea"
+                className="materialize-textarea"
               />
-              <label for="description">Description</label>
+              <label htmlFor="description">Description</label>
             </div>
-            <div class="input-field col s6">
-              <input id="name" type="number" name="price" class="validate" />
-              <label for="name">Price</label>
+            <div className="input-field col s6">
+              <input
+                id="price"
+                type="number"
+                name="price"
+                className="validate"
+                min={0}
+              />
+              <label htmlFor="price">Price</label>
             </div>
-            <div class="input-field col s6">
+            <div className="input-field col s6">
               <input
                 id="stocked"
                 type="number"
                 name="stocked"
-                class="validate"
+                className="validate"
+                min={0}
               />
-              <label for="stocked">Stocked</label>
+              <label htmlFor="stocked">Stocked</label>
             </div>
 
             <div className="row">
-              <div class="input-field col s6">
-                <input id="author" name="author" type="text" class="validate" />
-                <label for="author">Author</label>
+              <div className="input-field col s6">
+                <input
+                  id="author"
+                  name="author"
+                  type="text"
+                  className="validate"
+                />
+                <label htmlFor="author">Author</label>
               </div>
-              <div class="input-field col s6">
-                <input id="type" name="type" type="text" class="validate" />
-                <label for="type">Type</label>
+              <div className="input-field col s6">
+                <input id="type" name="type" type="text" className="validate" />
+                <label htmlFor="type">Type</label>
               </div>
             </div>
 
             <div className="row">
-              <div class="input-field col s6">
-                <input id="img" name="img" type="text" class="validate" />
-                <label for="img">Image URL</label>
+              <div className="input-field col s6">
+                <input id="img" name="img" type="text" className="validate" />
+                <label htmlFor="img">Image URL</label>
               </div>
-              <div class="input-field col s6">
+              <div className="input-field col s6">
                 <input
                   id="category"
                   name="category"
                   type="text"
-                  class="validate"
+                  className="validate"
                 />
-                <label for="category">Category</label>
+                <label htmlFor="category">Category</label>
               </div>
             </div>
             <button
-              class="btn waves-effect waves-light col s12"
+              className="btn waves-effect waves-light col s12"
               type="submit"
               name="action"
             >
