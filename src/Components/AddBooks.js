4c8b90f8dddcc64ffa533c36faa5140fc84b598f
poornesh-pH <poornesh9626@gmail.com>
2021-06-26 13:46:52 +0530

    3 files modified

--- src/Components/AddBooks.js
+++ src/Components/AddBooks.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import BooksForm from './BooksForm';
-import {connect} from 'react-redux';
-import {addProduct} from '../Redux/Actions/productsAction';
-const AddBooks = (props)=>{
-  return(
+import { connect } from 'react-redux';
+import { addProduct } from '../Redux/Actions/productsAction';
+const AddBooks = props => {
+  return (
     <div className="container">
-    <h5>Add Books</h5>
-    <BooksForm onSubmit={(newBook)=>{
-      props.dispatch(addProduct(newBook))
-      props.history.push('/admin')
-    }}/>
+      <h5>Add Books</h5>
+      <BooksForm
+        onSubmit={newBook => {
+          props.dispatch(addProduct(newBook));
+          props.history.push('/admin');
+        }}
+      />
     </div>
-  )
-}
-export default connect()(AddBooks)
+  );
+};
+export default connect()(AddBooks);
--- src/Components/AdminBookList.js
+++ src/Components/AdminBookList.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 const AdminBookList = props => {
-  return <div>
+  return <div className="adminBookList">
   <Link to={`/edit/${props.book.id}`}>{props.book.name}</Link>
+<a class="waves-effect waves-light btn-small"><i class="tiny material-icons right">delete</i>Delete</a>
+
   </div>;
 };
 export default AdminBookList;
