17f7a8a8e7720427b46a15973bfbb6a5a387e9c8
poornesh-pH <poornesh9626@gmail.com>
2021-06-26 14:18:24 +0530

    2 files modified

--- src/Components/AdminBookList.js
+++ src/Components/AdminBookList.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { removeProduct } from '../Redux/Actions/productsAction';
 
 const AdminBookList = props => {
-  return <div className="adminBookList">
-  <Link to={`/edit/${props.book.id}`}>{props.book.name}</Link>
-<a class="waves-effect waves-light btn-small"><i class="tiny material-icons right">delete</i>Delete</a>
-
-  </div>;
+  return (
+    <div className="adminBookList">
+      <Link to={`/edit/${props.book.id}`}>{props.book.name}</Link>
+      <a
+        className="waves-effect waves-light "
+        onClick={() => {
+          props.dispatch(removeProduct(props.book.id));
+        }}
+      >
+        Delete
+      </a>
+    </div>
+  );
 };
-export default AdminBookList;
+export default connect()(AdminBookList);
