fc3f16d17b4c39fcdf8ae0ba98759fc75f805ec0
poornesh-pH <poornesh9626@gmail.com>
2021-06-27 22:09:53 +0530

    1 file modified

--- src/Components/BooksCards.js
+++ src/Components/BooksCards.js
@@ -9,11 +9,11 @@ const BooksCards = props =>
           {props.books.map(item => (
             <div className="card">
               <div className="card-image waves-effect waves-block waves-light">
-                <img className="activator" src="images/office.jpg" />
+                <img className="activator" src={item.img} />
               </div>
               <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4">
-                  Card Title<i className="material-icons right">more_vert</i>
+                  {item.name}<i className="material-icons right">more_vert</i>
                 </span>
                 <p>
                   <a href="#">This is a link</a>
@@ -21,11 +21,10 @@ const BooksCards = props =>
               </div>
               <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4">
-                  Card Title<i className="material-icons right">close</i>
+                 {item.name}<i className="material-icons right">close</i>
                 </span>
                 <p>
-                  Here is some more information about this product that is only
-                  revealed once clicked on.
+                 {item.description}
                 </p>
               </div>
             </div>
