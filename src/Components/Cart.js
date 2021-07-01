acc5ee1fb553138bcab0625eaa414acc008f27e5
poornesh-pH <poornesh9626@gmail.com>
2021-07-01 13:18:54 +0530

    1 file modified

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
-export default function Cart() {
+import {connect} from 'react-redux';
+function Cart(props) {
+  console.log(props,"Cart Component")
   return (
     <div className="container">
       <h6>Cart</h6>
     </div>
   );
 }
+const filterProductsToCart = (products,cart) =>{
+return products.find((item)=>item.id === cart.id)
+}
+const mapStateToProps =(state)=>{
+  return{
+    itemsInCart : filterProductsToCart(state.products,state.cart)
+  }
+}
+export default connect(mapStateToProps)(Cart);
